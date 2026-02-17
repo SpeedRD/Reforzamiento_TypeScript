8ab373cd60e03e18af3cd643b73b93f97451d44f
Eduardo Jose Cabreja Perez <eduardojose.cabreja@babelgroup.com>
2026-02-17 11:41:25 +0100

    Initial Commit

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName: string = 'Max'
+const lastName = 'Verstappen'
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterV = lastName.includes('V');
+
+console.log({containsLetterV, diceNumber, firstName, lastName});
